0046e0e1845de1e04825ce5f2e23e14f6e73ce27
Niall McGuinness <niall.mcguinness@dkit.ie>
2025-04-10 12:47:46 +0100

    Create 1_Exercises.R

---
+++ Scripts/Exercises/1_Exercises.R
@@ -0,0 +1,7 @@
+#' @title 1_Exercises.R
+#' @description Sandbox exercises for R programming, including data manipulation and visualization.
+#' @author NMCG
+#' @bugs None
+
+# Automatically clear console - the string literal "\014" sends a CTRL + L to the console to clear it
+cat("\014")
